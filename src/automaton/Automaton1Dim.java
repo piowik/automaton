d2454bc36a96cdcd1428ccc91c49bd174cdd3e3c
piowik <piotrwiklinski@gmail.com>
2018-01-23 16:17:43 +0100

    1Dim 2Dim methods and fields

--- src/automaton/Automaton1Dim.java
+++ src/automaton/Automaton1Dim.java
@@ -1,4 +1,17 @@
 package automaton;
 
 public abstract class Automaton1Dim extends Automaton {
+    private int size;
+
+    protected boolean hasNextCoordinates(CellCoordinates cell) {
+        // TODO: Automaton1Dim: hasNextCoordinates
+    }
+
+    protected CellCoordinates initialCoordinates(CellCoordinates cell) {
+        // TODO: Automaton1Dim: initialCoordinates
+    }
+
+    protected CellCoordinates nextCoordinates(CellCoordinates cell) {
+        // TODO: Automaton1Dim: nextCoordinates
+    }
 }
--- src/automaton/Automaton2Dim.java
+++ src/automaton/Automaton2Dim.java
@@ -1,4 +1,18 @@
 package automaton;
 
 public abstract class Automaton2Dim extends Automaton {
+    private int width;
+    private int height;
+
+    protected boolean hasNextCoordinates(CellCoordinates cell) {
+        // TODO: Automaton2Dim: hasNextCoordinates
+    }
+
+    protected CellCoordinates initialCoordinates(CellCoordinates cell) {
+        // TODO: Automaton2Dim: initialCoordinates
+    }
+
+    protected CellCoordinates nextCoordinates(CellCoordinates cell) {
+        // TODO: Automaton2Dim: nextCoordinates
+    }
 }
