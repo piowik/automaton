ab044bb806804c1a8cf46dd4069e716d6b6c43ac
mpodles <mp@kpodles.pl>
2018-01-23 17:22:32 +0100

    Automaton -constructor

--- src/automaton/Automaton.java
+++ src/automaton/Automaton.java
@@ -4,29 +4,37 @@ import java.util.Map;
 import java.util.Set;
 
 public abstract class Automaton {
-   private Map<CellCoordinates,CellState> cells;
 
-   private CellNeighborhood neighborsStrategy;
 
-   private CellStateFactory stateFactoryl;
+    public Automaton(Map<CellCoordinates,CellState> cells, CellNeighborhood neighborsStrategy, CellStateFactory stateFactory)
+    {
+        this.cells= cells;
+        this.neighborsStrategy=neighborsStrategy;
+        this.stateFactory=stateFactory;
+    }
 
+    private Map<CellCoordinates, CellState> cells;
+
+    private CellNeighborhood neighborsStrategy;
+
+    private CellStateFactory stateFactory;
 
 
     public Automaton nextState() {
         //TODO
     }
 
-    public void insertStructure(Map<? extends CellCoordinates, ? extends CellState> structure){
+    public void insertStructure(Map<? extends CellCoordinates, ? extends CellState> structure) {
         //TODO
     }
 
 
-    public CellIterator cellIterator(){
+    public CellIterator cellIterator() {
         //TODO
     }
 
 
-    protected abstract Automaton newInstance(CellStateFactory cellStateFactory ,CellNeighborhood cellNeighborhood);
+    protected abstract Automaton newInstance(CellStateFactory cellStateFactory, CellNeighborhood cellNeighborhood);
 
     protected abstract boolean hasNextCoordinates(CellCoordinates coords);
 
@@ -36,20 +44,23 @@ public abstract class Automaton {
 
     protected abstract CellState nextCellState(CellState currentState, Set<Cell> neighborsStates);
 
-    private Set<Cell> mapCoordinates(Set<CellCoordinates> coords){
+    private Set<Cell> mapCoordinates(Set<CellCoordinates> coords) {
         //TODO
     }
 
     class CellIterator {
         private CellState currentState; // TODO: rozbieżność
+
         public boolean hasNext() {
             // TODO: hasNext
             return true;
         }
+
         public Cell next() {
             // TODO: next
             return new Cell();
         }
+
         public void setState(CellState newState) {
             currentState = newState;
         }
