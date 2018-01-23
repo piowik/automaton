d4e2e29f430a53b99a30d45fb5174a02ecf808ca
mpodles <mp@kpodles.pl>
2018-01-23 17:01:13 +0100

    Automaton class - all variables and functions defined

--- src/automaton/Automaton.java
+++ src/automaton/Automaton.java
@@ -1,17 +1,44 @@
 package automaton;
 
 import java.util.Map;
+import java.util.Set;
 
 public abstract class Automaton {
    private Map<CellCoordinates,CellState> cells;
 
    private CellNeighborhood neighborsStrategy;
 
+   private CellStateFactory stateFactoryl;
+
+
 
     public Automaton nextState() {
         //TODO
     }
 
+    public void insertStructure(Map<? extends CellCoordinates, ? extends CellState> structure){
+
+    }
+
+
+    public CellIterator cellIterator(){
+        //TODO
+    }
+
+
+    protected abstract Automaton newInstance(CellStateFactory ,CellNeighborhood );
+
+    protected abstract boolean hasNextCoordinates(CellCoordinates coords);
+
+    protected abstract CellCoordinates initialCoordinates(CellCoordinates coords);
+
+    protected abstract CellCoordinates nextCoordinates(CellCoordinates coords);
+
+    protected abstract CellState nextCellState(CellState currentState, Set<Cell> neighborsStates);
+
+    private Set<Cell> mapCoordinates(Set<CellCoordinates> coords){
+        //TODO
+    }
 
     class CellIterator {
         private CellState currentState; // TODO: rozbieżność
