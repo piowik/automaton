2c99063572e50cc51907431e50c176aa6ea6f32c
piowik <piotrwiklinski@gmail.com>
2018-01-24 21:17:39 +0100

    Coords1D and Coords2D overrides equals and hashCode methods

--- src/automaton/Coords1D.java
+++ src/automaton/Coords1D.java
@@ -6,4 +6,23 @@ public class Coords1D implements CellCoordinates  {
     Coords1D(int x) {
         this.x = x;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Coords1D other = (Coords1D) obj;
+        if (other.x == x)
+            return true;
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return x;
+    }
 }
