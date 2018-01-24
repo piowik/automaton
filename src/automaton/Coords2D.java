b13a314185f45f7e3e7532e1dbceaee3c05ae26d
piowik <piotrwiklinski@gmail.com>
2018-01-24 23:14:38 +0100

    Nope. It does not require equals and hash...

--- src/automaton/Coords2D.java
+++ src/automaton/Coords2D.java
@@ -8,23 +8,4 @@ public class Coords2D implements CellCoordinates {
         this.x = x;
         this.y = y;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Coords2D other = (Coords2D) obj;
-        if (other.x == x && other.y == y)
-            return true;
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return (1000 * x) + y; // TODO: is 1000 enough? another way?
-    }
 }
