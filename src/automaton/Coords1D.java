68415312936a3454b1d27acbbcd82bfd78c5fb84
piowik <piotrwiklinski@gmail.com>
2018-01-24 11:57:51 +0100

    Coords constructors

--- src/automaton/Coords1D.java
+++ src/automaton/Coords1D.java
@@ -2,4 +2,8 @@ package automaton;
 
 public class Coords1D implements CellCoordinates  {
     public int x;
+
+    Coords1D(int x) {
+        this.x = x;
+    }
 }
--- src/automaton/Coords2D.java
+++ src/automaton/Coords2D.java
@@ -3,4 +3,9 @@ package automaton;
 public class Coords2D implements CellCoordinates {
     public int x;
     public int y;
+
+    Coords2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
