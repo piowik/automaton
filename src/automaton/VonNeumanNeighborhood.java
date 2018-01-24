0c73c296d687c01b443c092605c3de4fbf4d9fef
mpodles <mp@kpodles.pl>
2018-01-24 12:09:35 +0100

    Von Neuman without wrap

--- src/automaton/VonNeumanNeighborhood.java
+++ src/automaton/VonNeumanNeighborhood.java
@@ -6,15 +6,26 @@ import java.util.TreeSet;
 public class VonNeumanNeighborhood implements CellNeighborhood{
 
 
-    VonNeumanNeighborhood(int width, int height) {
+    VonNeumanNeighborhood(int width, int height, boolean wrap, int r) {
         this.width=width;
         this.height=height;
+        this.weap=wrap;
+        this.r=r;
     }
     private int width;
     private int height;
+    private boolean weap;
+    private int r;
 
-public Set<CellCoordinates> cellNeighbors(CellCoordinates cell){
+public Set<CellCoordinates> cellNeighbors(CellCoordinates cellCoordinates){
     Set<CellCoordinates> neighbors = new TreeSet<>();
+    Coords2D coords = (Coords2D) cellCoordinates;
+    for(int i=0;i<width;i++)
+        for(int j=0;j<height;j++)
+            if((Math.abs(i-coords.x)+Math.abs(j-coords.y))<=r)
+                neighbors.add(new Coords2D(i,j));
+
+    return neighbors;
  }
 
 }
